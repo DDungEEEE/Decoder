26c0404673096355adad6dfd363fcd3242e6a747
DDungEEEE <thsdnjsdlr12@gmail.com>
2024-01-23 16:35:44 +0900

    updaate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Welcome(props){
+  return <h1>Hello, {props.name}</h1>
 }
 
-export default App;
+function App(props){
+  return(
+    <div>
+      <Welcome name = "박민서"/>
+      <Welcome name = "겁나"/>
+      <Welcome name = "노싸가지"/>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+)
